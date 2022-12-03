bbb72fcce7ce6b4eabfce4f96827bc91d2599ef6
Andrey Sorokin <ands@list.ru>
2022-12-03 20:16:28 +0400

    Made a method that creates a string of words

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿string[] CreateInitialArray()
+{
+    Console.Write("Input the number of words: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+
+    string[] arrayInitial = new string[size];
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write("Enter the word: ");
+        arrayInitial[i] = Console.ReadLine();
+
+        if(arrayInitial[i] == string.Empty) 
+        {
+            Console.WriteLine("You didn't enter a word. Please, enter the word: ");
+        }
+    }
+    return arrayInitial;
+}
+
+
+
+
+string[] initialArray = CreateInitialArray();
